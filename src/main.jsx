e5e7fb6b9e7385961d32bbc20267c580b1966dfb
Vladimir <adda37@yandex.ru>
2025-04-13 14:15:04 +0300

    FIX main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import './index.css'
 const root = createRoot(document.querySelector('#root'))
 import routes from "./routes"
 
-createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-  </>,
+root.render(
+    <RouterProvider router={routes}/>
 )
