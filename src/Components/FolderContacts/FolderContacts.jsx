1e7f26c41bc2a47ee09dc675bd9b97937c83dbff
Timur <abkeevtimur@gmail.com>
2025-04-27 10:19:29 +0300

    AboutMe

--- src/Components/FolderContacts/FolderContacts.jsx
+++ src/Components/FolderContacts/FolderContacts.jsx
@@ -19,7 +19,7 @@ export default function FolderContacts(){
                         <svg className={s.chapter_arrow} xmlns="http://www.w3.org/2000/svg" width="9" height="7" viewBox="0 0 9 7" fill="none">
                             <path d="M4.74998 6.65186L0.499969 0.651856L9 0.651855L4.74998 6.65186Z" fill="white" />
                         </svg>
-                        <h2 className={s.chapter_title}>{data.contacts.title}</h2>
+                        <h2 className={`${s.chapter_title} ${s.titles}`}>{data.contacts.title}</h2>
                     </div>
                     <div className={s.folders}>
                         {data.contacts.section.map((el, i) => {
